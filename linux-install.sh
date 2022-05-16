c4800b3f72bccd41d1de3cb4bd02b77060ae2a45
opoolminer <101661239+opoolminer@users.noreply.github.com>
2022-05-17 02:38:05 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -187,7 +187,7 @@ show_menu() {
      echo -e "
      ${green}$uiname脚本管理界面安装完成${red}版本${shell_version},转发软件版本${version}
      ${green}任意目录下输入$uiname 启动管理界面
-     ${red}转发软件浏览器默认端口62438,默认用户名密码admin,admin${plain}
+     ${red}软件浏览器默认端口62438,默认用户名密码admin,admin${plain}
 
      ${green}0.${plain} 退出
      ${green}1.${plain} 安装
