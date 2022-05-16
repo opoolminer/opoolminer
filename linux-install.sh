baf795ef89dc1fbf857b0367a87ee219250606fe
opoolminer <101661239+opoolminer@users.noreply.github.com>
2022-05-17 02:16:48 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -1,419 +1,229 @@
-#!/bin/bash
+#bin
+version='7.0.1.3'
+uiname='xxminerproxy-ui'
+pkgname='xxminerproxy'
+authorname='klpool'
+installname='install.sh'
+shell_version='3.0.0'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+myFile=$version.tar.gz
+installfolder='/etc/porttran'
 
-VERSION="1.0.2"
-
-DOWNLOAD_HOST="https://github.com/opoolminer/opoolminer/raw/main/linux"
-
-PATH_OPOOL="/root/opool"
-
-PATH_EXEC="opool"
-
-PATH_CACHE="/root/opool/.cache"
-
-PATH_CONFIG="/root/opool/.env"
-
-PATH_NOHUP="/root/opool/nohup.out"
-PATH_ERR="/root/opool/err.log"
-
-
-PATH_TURN_ON="/etc/profile.d"
-PATH_TURN_ON_SH="/etc/profile.d/ktm.sh"
-
-ISSUE() {
-    echo "1.0.0"
-    echo "1.0.2"
-
-}
-
-colorEcho(){
-    COLOR=$1
-    echo -e "\033[${COLOR}${@:2}\033[0m"
-}
-
-filterResult() {
-    if [ $1 -eq 0 ]; then
-        echo ""
-    else
-        colorEcho ${RED} "【${2}】失败。"
-	
-        if [ ! $3 ];then
-            colorEcho ${RED} "!!!!!!!!!!!!!!!ERROR!!!!!!!!!!!!!!!!"
-            exit 1
-        fi
-    fi
-    echo -e
-}
-
-getConfig() {
-    value=$(sed -n 's/^[[:space:]]*'$1'[[:space:]]*=[[:space:]]*\(.*[^[:space:]]\)\([[:space:]]*\)$/\1/p' $PATH_CONFIG)
-    echo $value
-}
-
-setConfig() {
-    if [ ! -f "$PATH_CONFIG" ]; then
-        echo "未发现环境变量配置文件, 创建.env"
-        
-        touch $PATH_CONFIG
-
-        chmod -R 777 $PATH_CONFIG
-
-        echo "KT_START_PORT=21888" >> $PATH_CONFIG
-    fi
-
-    TARGET_VALUE="$1=$2"
-
-    line=$(sed -n '/'$1'/=' ${PATH_CONFIG})
-
-    sed -i "${line} a $TARGET_VALUE" $PATH_CONFIG
-
-    sed  -i  "$line d" $PATH_CONFIG
-
-    colorEcho ${GREEN} "$1已修改为$2"
-}
-
-#检查是否为Root
-[ $(id -u) != "0" ] && { colorEcho ${RED} "请使用root用户执行此脚本."; exit 1; }
-
-PACKAGE_MANAGER="apt-get"
-PACKAGE_PURGE="apt-get purge"
-
-#######color code########
-RED="31m"
-GREEN="32m"
-YELLOW="33m"
-BLUE="36m"
-FUCHSIA="35m"
-
-if [[ `command -v apt-get` ]];then
-    PACKAGE_MANAGER='apt-get'
-elif [[ `command -v dnf` ]];then
-    PACKAGE_MANAGER='dnf'
-elif [[ `command -v yum` ]];then
-    PACKAGE_MANAGER='yum'
-    PACKAGE_PURGE="yum remove"
+if [ ! -f "$myFile" ]; then
+echo "\n"
 else
-    colorEcho $RED "不支持的操作系统."
-    exit 1
+rm $version.tar.gz
 fi
 
-checkProcess() {
-    COUNT=$(ps -ef |grep $1 |grep -v "grep" |wc -l)
-
-    if [ $COUNT -eq 0 ]; then
-        return 0
-    else
-        return 1
-    fi
+kill_porttran(){
+      PROCESS=`ps -ef|grep porttran|grep -v grep|grep -v PPID|awk '{ print $2}'`
+      for i in $PROCESS
+      do
+        echo "Kill the $1 process [ $i ]"
+        kill -9 $i
+      done
+}
+kill_ppexec(){
+      PROCESS=`ps -ef|grep ppexec|grep -v grep|grep -v PPID|awk '{ print $2}'`
+      for i in $PROCESS
+      do
+        echo "Kill the $1 process [ $i ]"
+        kill -9 $i
+      done
+}
+install() {
+   wget https://github.com/$authorname/$pkgname/archive/refs/tags/$version.tar.gz
+   tar -zxvf $version.tar.gz
+   cd $pkgname-$version/porttranpay
+   tar -zxvf porttranlatest.tar.gz
+   cd ../..
+   mv $pkgname-$version/porttranpay/porttran/portdir.sh $pkgname-$version/porttranpay/porttran/porttran
+   mkdir porttran && chmod 777 porttran
+   mv $pkgname-$version/porttranpay/porttran/* porttran
+   cd porttran/ && chmod +x porttran && chmod +x ppexec
+   cd ../
+   rm -rf $pkgname-$version
+   rm $version.tar.gz
+   rm $installname
+   cp -r porttran /etc/
+   rm -rf porttran/
+   cd /etc/security/
+   echo "* soft nofile 20000" >> limits.conf
+   echo "* hard nofile 20000" >> limits.conf
+   cd /etc/pam.d/
+   echo "session required /lib/security/pam_limits.so" >> login
+   echo "session required /lib64/security/pam_limits.so" >> login
+   cd /etc
+   rm sysctl.conf
+   touch sysctl.conf
+   chmod 777 sysctl.conf
+   echo "net.ipv4.ip_local_port_range = 1024 65535" >> sysctl.conf
+   echo "net.core.rmem_max=16777216" >> sysctl.conf
+   echo "net.core.wmem_max=16777216" >> sysctl.conf
+   echo "net.ipv4.tcp_rmem=4096 87380 16777216" >> sysctl.conf
+   echo "net.ipv4.tcp_wmem=4096 65536 16777216" >> sysctl.conf
+   echo "net.ipv4.tcp_fin_timeout = 10" >> sysctl.conf
+   echo "net.ipv4.tcp_tw_recycle = 1" >> sysctl.conf
+   echo "net.ipv4.tcp_timestamps = 0" >> sysctl.conf
+   echo "net.ipv4.tcp_window_scaling = 0" >> sysctl.conf
+   echo "net.ipv4.tcp_sack = 0" >> sysctl.conf
+   echo "net.core.netdev_max_backlog = 30000" >> sysctl.conf
+   echo "net.ipv4.tcp_no_metrics_save=1" >> sysctl.conf
+   echo "net.core.somaxconn = 262144" >> sysctl.conf
+   echo "net.ipv4.tcp_syncookies = 0" >> sysctl.conf
+   echo "net.ipv4.tcp_max_orphans = 262144" >> sysctl.conf
+   echo "net.ipv4.tcp_max_syn_backlog = 262144" >> sysctl.conf
+   echo "net.ipv4.tcp_synack_retries = 2" >> sysctl.conf
+   echo "net.ipv4.tcp_syn_retries = 2" >> sysctl.conf
+   /sbin/sysctl -p /etc/sysctl.conf
+   /sbin/sysctl -w net.ipv4.route.flush=1
+   echo ulimit -HSn 65535 >> /ect/rc.local
+   echo ulimit -Hsn 65535 >> /root/.bash_profile
+   ulimit -Hsn 65535 
+   clear
+   before_show_menu
 }
 
-clearlog() {
-    echo "清理日志"
-    rm $PATH_NOHUP > /dev/null 2>&1
-    rm $PATH_ERR > /dev/null 2>&1
-    echo "清理完成"
+check_install() {
+if [ ! -d "$installfolder" ]; then
+  echo -e "             ${red}<<转发没有安装>>"
+  else
+  echo -e "             ${green}<<转发已经安装>>"
+fi
 }
 
-stop() {
-    colorEcho $BLUE "终止OPOOL进程"
-    killall opool
-    sleep 1
+before_show_menu() {
+    echo && echo -n -e "${yellow}操作完成按回车返回主菜单: ${plain}" && read temp
+    show_menu
 }
-
-uninstall() {    
-    stop
-
-    rm -rf ${PATH_OPOOL}
-
-    turn_off
-
-    colorEcho $GREEN "卸载完成"
+update_shell() {
+  wget https://raw.githubusercontent.com/$authorname/$pkgname/main/$installname -O -> /usr/bin/$uiname && chmod +x /usr/bin/$uiname && $uiname
+  echo 
+  exit 0
 }
-
+update_app() {
+   kill_porttran
+   kill_ppexec
+   wget https://github.com/$authorname/$pkgname/archive/refs/tags/$version.tar.gz
+   tar -zxvf $version.tar.gz
+   cd $pkgname-$version/porttranpay
+   tar -zxvf porttranlatest.tar.gz
+   cd ../..
+   mv $pkgname-$version/porttranpay/porttran/portdir.sh $pkgname-$version/porttranpay/porttran/porttran
+   mkdir porttran && chmod 777 porttran
+   mv $pkgname-$version/porttranpay/porttran/* porttran
+   cd porttran/ && chmod +x porttran && chmod +x ppexec
+   cd ../
+   rm -rf $pkgname-$version
+   rm $version.tar.gz
+   rm $installname
+   rm /etc/porttran/porttran
+   rm /etc/porttran/ppexec
+   rm -rf /etc/porttran/ui
+   cp porttran/ppexec /etc/porttran/
+   cp porttran/porttran /etc/porttran/
+   cd porttran/
+   cp -r ui /etc/porttran
+   cd ../
+   rm -rf porttran/
+   before_show_menu
+}
+uninstall_app() {
+   echo && echo -n -e "${yellow}确定卸载吗,按回车确定,CTRL+C退出: ${plain}" && read temp
+   kill_porttran
+   kill_ppexec
+   rm -rf /etc/porttran
+   before_show_menu
+}
+uninstall_shell() {
+   echo && echo -n -e "${yellow}确定卸载吗,按回车确定,CTRL+C退出: ${plain}" && read temp
+   rm /usr/bin/$uiname
+   before_show_menu
+}
 start() {
-    colorEcho $BLUE "启动程序..."
-    checkProcess "opool"
-    if [ $? -eq 1 ]; then
-        colorEcho ${RED} "程序已经启动，请不要重复启动。"
-        return
-    else
-        # 要先cd进去 否则nohup日志会产生在当前路径
-        cd $PATH_OPOOL
-        filterResult $? "打开目录"
-
-        clearlog
-
-        nohup "${PATH_OPOOL}/${PATH_EXEC}" 2>err.log &
-        # nohup "${PATH_OPOOL}/${PATH_EXEC}" >/dev/null 2>log &
-        filterResult $? "启动程序"
-
-        # getConfig "KT_START_PORT"
-        port=$(getConfig "KT_START_PORT")
-
-        colorEcho $GREEN "程序启动成功, WEB访问端口${port}, 默认账号admin, 默认密码admin123。"
-    fi
+   cd /etc/porttran
+   setsid ./porttran &
+   sleep 2
+   before_show_menu
 }
-
-update() {
-    installapp {{1.0.2}}
+stop() {
+   echo && echo -n -e "${yellow}确定停止吗,按回车确定,CTRL+C退出: ${plain}" && read temp
+   kill_porttran
+   kill_ppexec
+   before_show_menu
 }
-
-turn_on() {
-    
-    if [ ! -f "$PATH_TURN_ON_SH" ];then
-
-        touch $PATH_TURN_ON_SH
-
-        chmod 777 -R $PATH_OPOOL
-        chmod 777 -R $PATH_TURN_ON
-
-        echo 'COUNT=$(ps -ef |grep '$PATH_EXEC' |grep -v "grep" |wc -l)' >> $PATH_TURN_ON_SH
-
-        echo 'if [ $COUNT -eq 0 ] && [ $(id -u) -eq 0 ]; then' >> $PATH_TURN_ON_SH
-        echo "  cd ${PATH_OPOOL}" >> $PATH_TURN_ON_SH
-        echo "  nohup "${PATH_OPOOL}/${PATH_EXEC}" 2>err.log &" >> $PATH_TURN_ON_SH
-        echo '  echo "OPOOL已启动"' >> $PATH_TURN_ON_SH
-        echo 'else' >> $PATH_TURN_ON_SH
-        echo '  if [ $COUNT -ne 0 ]; then' >> $PATH_TURN_ON_SH
-        echo '      echo "OPOOL已启动, 无需重复启动"' >> $PATH_TURN_ON_SH
-        echo '  elif [ $(id -u) -ne 0 ]; then' >> $PATH_TURN_ON_SH
-        echo '      echo "使用ROOT用户登录才能启动OPOOL"' >> $PATH_TURN_ON_SH
-        echo '  fi' >> $PATH_TURN_ON_SH
-        echo 'fi' >> $PATH_TURN_ON_SH
-
-        echo "已设置开机启动"
-    else
-        echo "已设置开机启动, 无需重复设置"
-    fi
+autorun() {
+      cd /etc
+      rm rc.local
+      touch rc.local
+      chmod 777 rc.local
+      echo "#!/bin/bash" >> rc.local
+      echo "#" >> rc.local
+      echo "# rc.local" >> rc.local
+      echo "#" >> rc.local
+      echo "# This script is executed at the end of each multiuser runlevel." >> rc.local
+      echo "# Make sure that the script will "#exit 0" on success or any other" >> rc.local
+      echo "# value on error." >> rc.local
+      echo "#" >> rc.local
+      echo "# In order to enable or disable this script just change the execution" >> rc.local
+      echo "# bits." >> rc.local
+      echo "#" >> rc.local
+      echo "# By default this script does nothing." >> rc.local
+      echo "#exit 0" >> rc.local
+      echo "cd /etc/porttran && setsid ./porttran &" >> rc.local
+      echo "exit 0" >> rc.local
+      cd /root
+      echo -e "${green}开机启动设置成功"
+      before_show_menu
 }
+show_menu() {
+   clear
+     check_install
+     echo -e "
+     ${green}$uiname脚本管理界面安装完成${red}版本${shell_version},转发软件版本${version}
+     ${green}任意目录下输入$uiname 启动管理界面
+     ${red}转发软件浏览器默认端口62438,默认用户名密码admin,admin${plain}
+   ————————————————
+     ${green}0.${plain} 退出
+   ————————————————
+     ${green}1.${plain} 安装转发
+     ${green}2.${plain} 更新转发
+     ${green}3.${plain} 卸载转发
+   ————————————————
+     ${green}4.${plain} 更新脚本
+     ${green}5.${plain} 卸载脚本
+   ————————————————
+     ${green}6.${plain} 启动转发
+     ${green}7.${plain} 停止转发
+     ${green}8.${plain} 开机启动
+   ————————————————
+   "
+    echo && read -p "请输入选择 [0-8]: " num
 
-turn_off() {
-    rm $PATH_TURN_ON_SH
-    echo "已关闭开机启动"
-}
-
-installapp() {
-    if [ -n "$1" ]; then
-        VERSION="$1"
-    fi
-    
-    colorEcho ${GREEN} "开始安装OPOOL"
-
-    if [[ `command -v yum` ]];then
-        colorEcho ${BLUE} "关闭防火墙"
-        systemctl stop firewalld.service 1>/dev/null
-        systemctl disable firewalld.service 1>/dev/null
-    fi
-
-    colorEcho $BLUE "是否更新LINUX软件源？如果您的LINUX更新过可输入2跳过并继续安装，如果您不了解用途直接输入1。"
-    read -p "$(echo -e "请选择[1-2]：")" choose
-    case $choose in
-    1)
-        colorEcho ${BLUE} "开始更新软件源..."
-        $PACKAGE_MANAGER update -y
-    ;;
+    case "${num}" in
+        0) exit 0
+        ;;
+        1) install
+        ;;
+        2) update_app
+        ;;
+        3) uninstall_app
+        ;;
+        4) update_shell
+        ;;
+        5) uninstall_shell
+        ;;
+        6) start
+        ;;
+        7) stop
+        ;;
+        8) autorun
+        ;;
+        *) echo -e "${red}请输入正确的数字 [0-8]${plain}"
+        ;;
     esac
-    
-    if [[ ! `command -v curl` ]];then 
-        echo "尚未安装CURL, 开始安装"
-        $PACKAGE_MANAGER install curl
-    fi
-
-    if [[ ! `command -v wget` ]];then
-        echo "尚未安装wget, 开始安装"
-        $PACKAGE_MANAGER install wget
-    fi
-
-    if [[ ! `command -v killall` ]];then
-        echo "尚未安装killall, 开始安装"
-        $PACKAGE_MANAGER install psmisc
-    fi
-
-    if [[ ! `command -v killall` ]];then
-        colorEcho ${RED} "安装killall失败！！！！请手动安装psmisc后再执行安装程序。"
-        return
-    fi
-
-    checkProcess "opool"
-    if [ $? -eq 1 ]; then
-        colorEcho ${RED} "发现正在运行的OPOOL, 需要停止才可继续安装。"
-        colorEcho ${YELLOW} "输入1停止正在运行的OPOOL并且继续安装, 输入2取消安装。"
-
-        read -p "$(echo -e "请选择[1-2]：")" choose
-        case $choose in
-        1)
-            stop
-            ;;
-        2)
-            echo "取消安装"
-            return
-            ;;
-        *)
-            echo "输入错误, 取消安装。"
-            return
-            ;;
-        esac
-    fi
-
-    colorEcho $BLUE "创建目录"
-    
-    if [[ ! -d $PATH_OPOOL ]];then
-        mkdir $PATH_OPOOL
-        chmod 777 -R $PATH_OPOOL
-    else
-        colorEcho $YELLOW "目录已存在, 无需重复创建, 继续执行安装。"
-    fi
-
-    if [[ ! -d $PATH_NOHUP ]];then
-        touch $PATH_NOHUP
-        touch $PATH_ERR
-
-        chmod 777 -R $PATH_NOHUP
-        chmod 777 -R $PATH_ERR
-    fi
-
-    if [[ ! -d $PATH_CONFIG ]];then
-        setConfig KT_START_PORT 21888
-    fi
-
-    colorEcho $BLUE "拉取程序"
-    # wget -P $PATH_OPOOL "${DOWNLOAD_HOST}/${ORIGIN_EXEC}" -O "${PATH_OPOOL}/${PATH_EXEC}" 1>/dev/null
-    wget -P $PATH_OPOOL "${DOWNLOAD_HOST}/opool-${VERSION}_linux" -O "${PATH_OPOOL}/${PATH_EXEC}" 1>/dev/null
-
-    filterResult $? "拉取程序 opool-${VERSION}_linux"
-
-    chmod 777 -R "${PATH_OPOOL}/${PATH_EXEC}"
-
-    turn_on
-
-    change_limit
-
-    start
 }
-
-change_limit(){
-    colorEcho $BLUE "修改系统最大连接数"
-
-    changeLimit="n"
-
-    if [ $(grep -c "root soft nofile" /etc/security/limits.conf) -eq '0' ]; then
-        echo "root soft nofile 65535" >>/etc/security/limits.conf
-        echo "* soft nofile 65535" >>/etc/security/limits.conf
-        changeLimit="y"
-    fi
-
-    if [ $(grep -c "root hard nofile" /etc/security/limits.conf) -eq '0' ]; then
-        echo "root hard nofile 65535" >>/etc/security/limits.conf
-        echo "* hard nofile 65535" >>/etc/security/limits.conf
-        changeLimit="y"
-    fi
-
-    if [ $(grep -c "DefaultLimitNOFILE=65535" /etc/systemd/user.conf) -eq '0' ]; then
-        echo "DefaultLimitNOFILE=65535" >>/etc/systemd/user.conf
-        changeLimit="y"
-    fi
-
-    if [ $(grep -c "DefaultLimitNOFILE=65535" /etc/systemd/system.conf) -eq '0' ]; then
-        echo "DefaultLimitNOFILE=65535" >>/etc/systemd/system.conf
-        changeLimit="y"
-    fi
-
-    if [[ "$changeLimit" = "y" ]]; then
-        echo "连接数限制已修改为65535,重启服务器后生效"
-    else
-        echo -n "当前连接数限制："
-        ulimit -n
-    fi
-}
-
-check_limit() {
-    echo "当前系统连接数：" 
-    ulimit -n
-}
-
-check_hub() {
-    # cd $PATH_OPOOL
-    colorEcho ${YELLOW} "按住CTRL+C后台运行"
-    tail -f /root/opool/nohup.out
-}
-
-check_err() {
-    colorEcho ${YELLOW} "按住CTRL+C后台运行"
-    tail -f /root/opool/err.log
-}
-
-install_target() {
-    echo "输入已发布的版本来进行安装："
-    echo ""
-    ISSUE
-    echo ""
-    read -p "$(echo -e "请输入版本号：")" choose
-
-    installapp $choose
-}
-
-restart() {
-    stop
-
-    start
-}
-
-set_port() {
-    read -p "$(echo -e "请输入要设置的端口号：")" choose
-
-    setConfig KT_START_PORT $choose
-
-    stop
-
-    start
-}
-
-echo "-------------------------------------------------------"
-colorEcho ${GREEN} "欢迎使用OPOOL安装工具, 请输入操作号继续。"
-
-echo ""
-echo "1、安装"
-echo "2、卸载"
-echo "3、更新"
-echo "4、启动"
-echo "5、重启"
-echo "6、停止"
-echo "7、修改启动端口"
-echo "8、解除linux系统连接数限制(需要重启服务器生效)"
-echo "9、查看当前系统连接数限制"
-echo ""
-echo "-------------------------------------------------------"
-
-read -p "$(echo -e "请选择[1-9]：")" choose
-
-case $choose in
-1)
-    installapp 1.0.2
-    ;;
-2)
-    uninstall
-    ;;
-3)
-    update
-    ;;
-4)
-    start
-    ;;
-5)
-    restart
-    ;;
-6)
-    stop
-    ;;
-7)
-    set_port
-    ;;
-8)
-    change_limit
-    ;;
-9)
-    check_limit
-    ;;
-*)
-    echo "输入了错误的指令, 请重新输入。"
-    ;;
-esac
+show_menu
